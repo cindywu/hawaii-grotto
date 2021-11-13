1fec11fe2f8f1615af0f4b7ae2788c64c3f47eaa
cindy <ciindy.wu@gmail.com>
2021-11-12 18:45:50 -1000

    doing it live

---
+++ components/nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './nav.module.css'
+
+export default function Nav() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.home}>
+        Hawaii Grotto
+      </div>
+    </div>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/home.module.css'
+import Nav from '../components/nav'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Hawaii Grotto</title>
+        <meta name="description" content="Explore caves on the Big Island" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Nav/>
+
+      <main className={styles.main}>
+        <div className={styles.hero}>
+          <h1 className={styles.title}>
+            Explore caves in <a>Hawaii</a>
+          </h1>
+        </div>
+
+
+        <p className={styles.description}>
+          Fly to Hawaii. Safely explore the underground. No experience required.
+        </p>
+
+
+      </main>
+    </div>
+  )
+}
+
+export default Home
